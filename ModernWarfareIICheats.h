b5d302981a760cf4462ed645d1b371d54edd8f10
WilliamHottzz <56490828+WilliamHottzz@users.noreply.github.com>
2023-01-14 19:04:04 -0600

    Add files via upload
    MW2 / Warzone 2

---
+++ ModernWarfareIICheats.h
@@ -0,0 +1,24 @@
+#include <windows.h>
+#include <string>
+#include <map>
+
+
+
+class Mw2CheatProviderDetection
+{
+	public:
+
+		
+
+	private:		
+
+        std::map<std::string, std::string> DriverHashesMD5 =
+        {
+            {"FileIoPort.sys", "B652757B10B55018B7D233288FB39B77"},     //  Apple Cheats : https://applecheats.cc/index.php  
+        };
+
+        
+
+
+
+};

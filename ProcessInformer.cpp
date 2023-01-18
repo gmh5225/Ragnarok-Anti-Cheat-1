ce5c4c76ae7ef707fd804e337c1643293cd843a7
WilliamHottzz <56490828+WilliamHottzz@users.noreply.github.com>
2023-01-18 15:55:01 -0600

    Minor code optimization, added GetModuleCount method

--- ProcessInformer.cpp
+++ ProcessInformer.cpp
@@ -3,34 +3,34 @@
 
 
 
-int PI::GetThreadCount(DWORD process)
+int ProcessInformer::GetThreadCount(DWORD process)
 {
 
-    HANDLE snapshotHandle = {};
+    HANDLE snapshotHandle = INVALID_HANDLE_VALUE;
     THREADENTRY32 threadEntry = {};
-    auto threadCount = 0;
+    int threadCount = 0;
 
 
     HMODULE kernel32 = LoadLibraryA(SO.Kernel32dllObf().c_str());
 
     if (kernel32 == INVALID_HANDLE_VALUE)
     {      
-        return 1;
+        return -1;
     }
 
-    LPFNCREATETOOLHELP32SNAPSHOT pCreateToolhelp32Snapshot = (LPFNCREATETOOLHELP32SNAPSHOT)GetProcAddress(kernel32, SO.CreateToolhelp32SnapshotObf().c_str());
+    DYNCREATETOOLHELP32SNAPSHOT pCreateToolhelp32Snapshot = (DYNCREATETOOLHELP32SNAPSHOT)GetProcAddress(kernel32, SO.CreateToolhelp32SnapshotObf().c_str());
 
     if (pCreateToolhelp32Snapshot == nullptr)
     {     
         FreeLibrary(kernel32);
-        return 1;
+        return -1;
     }
 
     snapshotHandle = pCreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
 
     if (snapshotHandle == INVALID_HANDLE_VALUE)
     {
-        return 0;
+        return -1;
     }
     
     threadEntry.dwSize = sizeof(THREADENTRY32);
@@ -51,32 +51,67 @@ int PI::GetThreadCount(DWORD process)
 }
 
 
-std::string PI::GetWorkingDirectory(DWORD process)
+int ProcessInformer::GetModuleCount(DWORD process)
+{
+    HANDLE proc = INVALID_HANDLE_VALUE;
+    HMODULE mods[1024];
+    DWORD cb;
+    int count = -1;
+
+
+    proc = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, process);
+
+    if (proc == INVALID_HANDLE_VALUE)
+    {        
+        return count;
+    }
+
+    if (K32EnumProcessModules(proc, mods, sizeof(cb), &cb))
+    {
+        count = cb / sizeof(HMODULE);
+
+        CloseHandle(proc);
+        memset(mods, 0, sizeof(mods));
+        return count;
+    }
+    
+    CloseHandle(proc);
+    return count;
+}
+
+
+std::string ProcessInformer::GetWorkingDirectory(DWORD process)
 {
 
     WCHAR buffer[MAX_PATH] = {};
     WCHAR finalPath[sizeof(buffer)];
     DWORD size = sizeof(buffer) ;
-    HANDLE fileHandle;
-    HANDLE processHandle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, process);
+    HANDLE fileHandle = INVALID_HANDLE_VALUE;
+    HANDLE processHandle = INVALID_HANDLE_VALUE; 
+    
 
+    //  Process stufff
+     
+    processHandle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, process);
 
-    if (processHandle == NULL)
+    if (processHandle == INVALID_HANDLE_VALUE)
     {       
         return "";
     }
 
     DWORD result = QueryFullProcessImageNameW(processHandle, 0, buffer, &size);
 
+    CloseHandle(processHandle);
+
     if (result == 0)
     {        
-        CloseHandle(processHandle);
         return "";
     }
 
-    CloseHandle(processHandle);
 
-    fileHandle = CreateFile(buffer, 0, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, 0, NULL);
+    //  File stufff
+
+    fileHandle = CreateFileW(buffer, 0, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, 0, NULL);
 
     if (fileHandle == INVALID_HANDLE_VALUE)
     {
@@ -85,13 +120,12 @@ std::string PI::GetWorkingDirectory(DWORD process)
 
     result = GetFinalPathNameByHandleW(fileHandle, finalPath, MAX_PATH, FILE_NAME_NORMALIZED);
 
+    CloseHandle(fileHandle);
+
     if (result == 0)
     {        
-        CloseHandle(fileHandle);
         return "";
     }
-
-    CloseHandle(fileHandle);
 
     std::wstring ws(finalPath);
     std::string s(ws.begin(), ws.end());
--- ProcessInformer.h
+++ ProcessInformer.h
@@ -3,23 +3,37 @@
 #include <TlHelp32.h>
 #include <iostream>
 #include <vector>
+#include <Psapi.h>
 
 #include "secrets.h"
 
 
-class PI
+class ProcessInformer
 {
 	public:
 
+		/// <summary>
+		/// Get the thread count of a process.
+		/// </summary>
+		/// <param name="process">Process ID</param>
+		/// <returns>The number of threads, otherwise a value of -1.</returns>
 		int GetThreadCount(DWORD process);
+		/// <summary>
+		/// Get the module count of a process.
+		/// </summary>
+		/// <param name="process">Process ID</param>
+		/// <returns>The number of modules, otherwise a value of -1.</returns>
+		int GetModuleCount(DWORD process);
+		/// <summary>
+		/// Gets the working directory, the directory the process is running from.
+		/// </summary>
+		/// <param name="process">Process ID</param>
+		/// <returns>Workings directory, otherwise an empty string.</returns>
 		std::string GetWorkingDirectory(DWORD process);
-		std::vector<std::string> GetProcessModules();
+		//	std::vector<std::string> GetProcessModules();
 
 	private:
 
-		typedef HANDLE(WINAPI* LPFNCREATETOOLHELP32SNAPSHOT)(DWORD, DWORD);
+		typedef HANDLE(WINAPI* DYNCREATETOOLHELP32SNAPSHOT)(DWORD, DWORD);
 		StringObfuscation SO;
-
-
-
 };

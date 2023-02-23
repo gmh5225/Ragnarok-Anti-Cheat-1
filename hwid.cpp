d9e03a01df28d072c2cdf17be87b366ca04faf64
WilliamHottzz <56490828+WilliamHottzz@users.noreply.github.com>
2023-02-23 13:12:15 -0600

    Add files via upload

--- hwid.cpp
+++ hwid.cpp
@@ -8,34 +8,48 @@ void SMBIOSHWID::ShowHwid()
 
     DWORD needBufferSize = 0;
     const DWORD Signature = 'RSMB';
-    LPBYTE pBuff = NULL;
+    LPBYTE buffer = NULL;
     
-    needBufferSize = GetSystemFirmwareTable(Signature, 0, NULL, 0); //  Cold Call
-    
-    pBuff = (LPBYTE)malloc(needBufferSize);
+    HMODULE kernel32 = LoadLibraryA(kernel32dllObf().c_str());
 
-    if (pBuff)
+    if (kernel32 == NULL)
     {
-        GetSystemFirmwareTable(Signature, 0, pBuff, needBufferSize);
-
-        const PRawSMBIOSData pDMIData = (PRawSMBIOSData)pBuff;        
-        DumpSMBIOSStruct(&(pDMIData->SMBIOSTableData), pDMIData->Length);
+        std::cout << "Hello, World!" << std::endl;
+        return;
     }
 
-    pBuff = NULL; 
+    PfGetSystemFirmwareTable ptrGetSystemFirmwareTable = (PfGetSystemFirmwareTable)GetProcAddress(kernel32, GetSystemFirmwareTableObf().c_str());
+
+    if (ptrGetSystemFirmwareTable == nullptr)
+    {
+        std::cout << "Hello, World!" << std::endl;
+        FreeLibrary(kernel32);
+    }
+
+    needBufferSize = ptrGetSystemFirmwareTable(Signature, 0, NULL, 0); //  Cold Call
+   
+    buffer = (LPBYTE)malloc(needBufferSize);
+
+    if (buffer)
+    {
+        ptrGetSystemFirmwareTable(Signature, 0, buffer, needBufferSize);
+
+        const PRawSMBIOSData pDMIData = (PRawSMBIOSData)buffer;        
+        DumpSMBIOSStruct(&(pDMIData->SMBIOSTableData), pDMIData->Length);
+    }
+    ptrGetSystemFirmwareTable = nullptr;
+    FreeLibrary(kernel32);
+    buffer = NULL; 
 }
 
 
-const char* SMBIOSHWID::toPointString(void* p)
-{
-    return (char*)p + ((PSMBIOSHEADER)p)->Length;
-}
+const char* SMBIOSHWID::toPointString(void* p){ return (char*)p + ((PSMBIOSHEADER)p)->Length; }
 
 
 const char* SMBIOSHWID::LocateStringA(const char* str, UINT i)
 {
 
-    static const char empty[] = "";
+    static const char empty[] = "No Data!";
 
     if (0 == i || 0 == *str) return empty;
 
@@ -63,66 +77,151 @@ const wchar_t* SMBIOSHWID::LocateStringW(const char* str, UINT i)
 }
 
 
-bool SMBIOSHWID::ProcSysInfo(void* p)
+bool SMBIOSHWID::ProcessorDetails(void* p)
 {
 
-    PSystemInfo sys = (PSystemInfo)p;
-    
-    
+    PProcessorInfo	pProcessor = (PProcessorInfo)p;
     const char* str = toPointString(p);
 
-    if (sys->Header.Length > 0x19)
-    {
-        //_tprintf(TEXT("SKU Number: %s\n"), LocateString(str, sys->SKUNumber));
-        MACHINE::SKU1 = LocateString(str, sys->SKUNumber);
-    }
-
-    if (sys->Header.Length > 0x08)
-    {
-       //_tprintf(TEXT("UUID: %02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X\n"),sys->UUID[0], sys->UUID[1], sys->UUID[2], sys->UUID[3],sys->UUID[4], sys->UUID[5], sys->UUID[6], sys->UUID[7],sys->UUID[8], sys->UUID[9], sys->UUID[10], sys->UUID[11],sys->UUID[12], sys->UUID[13], sys->UUID[14], sys->UUID[15]);       
-
-        wchar_t uuid[40];
-        swprintf(uuid, 40, TEXT("%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X"),sys->UUID[0], sys->UUID[1], sys->UUID[2], sys->UUID[3], sys->UUID[4], sys->UUID[5], sys->UUID[6], sys->UUID[7],sys->UUID[8], sys->UUID[9], sys->UUID[10], sys->UUID[11], sys->UUID[12], sys->UUID[13], sys->UUID[14], sys->UUID[15]);
-        MACHINE::UUID1 = uuid;
-    }
-
+    _tprintf(XORWIDE(TEXT("Processor Manufacturer: %s\n")), LocateString(str, pProcessor->Manufacturer));
+    uint8_t* id = (uint8_t*)&pProcessor->ID;
+    _tprintf(XORWIDE(TEXT("Processor ID: %02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X\n")),id[7], id[6], id[5], id[4], id[3], id[2], id[1], id[0]);
+    _tprintf(XORWIDE(TEXT("Processor Max Speed: %dMHz\n")), pProcessor->MaxSpeed);
+    _tprintf(XORWIDE(TEXT("Processor Current Speed: %dMHz\n")), pProcessor->CurrentSpeed);
     return true;
 }
 
 
-bool SMBIOSHWID::LogicBoardInfo(void* p)
+bool SMBIOSHWID::SystemDetails(void* p)
+{
+
+    PSystemInfo sys = (PSystemInfo)p;  
+    const char* str = toPointString(p);
+
+    if (sys->Header.Length == 0x0)
+    {            
+        return false;
+    }
+    if (sys->Header.Length > 0x08)
+    {
+        _tprintf(XORWIDE(TEXT("UUID: %02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X\n")),
+            sys->UUID[0], sys->UUID[1], sys->UUID[2], sys->UUID[3],
+            sys->UUID[4], sys->UUID[5], sys->UUID[6], sys->UUID[7],
+            sys->UUID[8], sys->UUID[9], sys->UUID[10], sys->UUID[11],
+            sys->UUID[12], sys->UUID[13], sys->UUID[14], sys->UUID[15]);
+    }
+    
+    if (sys->Header.Length > 0x19)
+    {
+        _tprintf(XORWIDE(TEXT("Product SKU Number: %s\n")), LocateString(str, sys->SKUNumber));       
+    }
+
+    _tprintf(XORWIDE(TEXT("Product Serial Number: %s\n")), LocateString(str, sys->SN));
+    _tprintf(XORWIDE(TEXT("Product Manufacturer: %s\n")), LocateString(str, sys->Manufacturer));
+    return true;
+}
+
+
+bool SMBIOSHWID::EnclosureDetails(void* p)
+{
+
+    PSystemEnclosure enclosure = (PSystemEnclosure)p;
+    const char* str = toPointString(p);
+
+    _tprintf(XORWIDE(TEXT("Enclosure Manufacturer: %s\n")), LocateString(str, enclosure->Manufacturer));
+    _tprintf(XORWIDE(TEXT("Enclosure Serial Number: %s\n")), LocateString(str, enclosure->SN));
+    return true;
+}
+
+
+bool SMBIOSHWID::LogicBoardDetails(void* p)
 {
 
     PBoardInfo pBoard = (PBoardInfo)p;
     const char* str = toPointString(p);
-   
-
-    //_tprintf(TEXT("Serial Number: %s\n"), LocateString(str, pBoard->SN));  
-    MACHINE::SRN1 = LocateString(str, pBoard->SN);
+    
+    _tprintf(XORWIDE(TEXT("Logic Board Serial Number: %s\n")), LocateString(str, pBoard->SN));
+    _tprintf(XORWIDE(TEXT("Logic Board Manufacturer: %s\n")), LocateString(str, pBoard->Manufacturer));
+    _tprintf(XORWIDE(TEXT("Logic Board ATN: %s\n")), LocateString(str, pBoard->AssetTag));
     return true;
 }
 
 
-bool SMBIOSHWID::DispatchStructType(PSMBIOSHEADER header)
+bool SMBIOSHWID::MemoryDeviceDetails(void* p)
 {
 
-    typedef struct
+    PMemoryDevice device = (PMemoryDevice)p;
+    const char* str = toPointString(p);
+
+    if (device->Header.Length > 0x15)
     {
-        BYTE t;
-        bool(*Proc)(void* p);
-    } TPFUNC;
+        _tprintf(XORWIDE(TEXT("Memory Device Serial Number: %s\n")), LocateString(str, device->SN));
+        _tprintf(XORWIDE(TEXT("Memory Device ATN: %s\n")), LocateString(str, device->AssetTag));
+        _tprintf(XORWIDE(TEXT("Memory Device Manufacturer: %s\n")), LocateString(str, device->Manufacturer));
+    }
+    return true;
+}
+
+
+bool SMBIOSHWID::NetworkDetails(void* p)
+{
+
+    POnboardDevicesExtendedInformation netdevice = (POnboardDevicesExtendedInformation)p;
+    const char* str = toPointString(p);
+    
+    _tprintf(XORWIDE(TEXT("Network Device Type: %u\n")), netdevice->DeviceType);
+    return true;
+}
+
+
+bool SMBIOSHWID::PortableBatteryDetails(void* p)
+{
+
+    PPortableBattery battery = (PPortableBattery)p;
+    const char* str = toPointString(p);
+
+    if (battery->SN != 0)
+    {
+        _tprintf(XORWIDE(TEXT("Serial Number: %s\n")), LocateString(str, battery->SN));
+    }
+
+    _tprintf(XORWIDE(TEXT("PBattery Manufacturer: %s\n")), LocateString(str, battery->Manufacturer));
+    return true;
+}
+    
+    
+bool SMBIOSHWID::StartStructureRoutine(PSMBIOSHEADER header)
+{
+    typedef struct{ BYTE t; bool(*Proc)(void* p);} TPFUNC;
 
     switch (header->Type)
     {
-    case 0:
-        ProcSysInfo((void*)header);
-        return true;
     case 1:
-        LogicBoardInfo((void*)header);
+        SystemDetails((void*)header);
+        return true;
+    case 2:
+        LogicBoardDetails((void*)header);
+        return true;
+    case 3:
+        EnclosureDetails((void*)header);
+        return true;
+    case 4:
+        ProcessorDetails((void*)header);
+        return true;
+    case 17:
+        MemoryDeviceDetails((void*)header);
+        return true;
+    case 22:
+        PortableBatteryDetails((void*)header);
+        return true;
+    case 41:
+        NetworkDetails((void*)header);
         return true;
     default:
         return false;
     }
+
+    return false;
 }
 
 
@@ -131,19 +230,87 @@ void SMBIOSHWID::DumpSMBIOSStruct(void* Addr, UINT Len)
   
     LPBYTE p = (LPBYTE)(Addr);
     const LPBYTE lastAddress = p + Len;
-    PSMBIOSHEADER pHeader;
+    PSMBIOSHEADER header;
+    bool isbiosempty = false;
+    bool issysempty = false;
+    bool isprocessorempty = false;
+    bool ismemempty = false;
+    bool islogicbempty = false;
+    bool isnetdeviceempty = false;
+    bool ispbatteryempty = false;
 
     for (;;)
     {
-        pHeader = (PSMBIOSHEADER)p;
-        DispatchStructType(pHeader);
+        header = (PSMBIOSHEADER)p;
+        StartStructureRoutine(header);
 
-        if ((pHeader->Type == 127) && (pHeader->Length == 4))
+        switch (header->Type) 
         {
-            break;
+        case 127:
+            if (header->Length == 4) 
+            {
+                return;
+            }
+           //break;
+        case 0:
+            if (header->Length == 0 && (isbiosempty == false))
+            {
+                std::cout << "\n---\tBIO structure appears to be empty\t---\n" << std::endl;
+                isbiosempty = true;
+                //continue;
+            }
+            //break;
+        case 1:
+            if (header->Length == 0 && (issysempty == false))
+            {
+                std::cout << "\n---\tSYS structure appears to be empty\t---\n" << std::endl;
+                issysempty = true;
+                //continue;
+            }
+            //break;
+        case 2:
+            if (header->Length == 0 && (islogicbempty == false))
+            {
+                std::cout << "\n---\tLOGIC BOARD structure appears to be empty\t---\n" << std::endl;
+                islogicbempty = true;
+                //continue;
+            }
+            //break;
+        case 4:
+            if (header->Length == 0 && (isprocessorempty == false))
+            {
+                std::cout << "\n---\tPROCESSOR structure appears to be empty\t---\n" << std::endl;
+                isprocessorempty = true;
+                //continue;
+            }
+            //break;
+        case 17:
+            if (header->Length == 0 && (ismemempty == false))
+            {
+                std::cout << "\n---\tMEMORY structure appears to be empty\t---\n" << std::endl;
+                ismemempty = true;
+                //continue;
+            }
+            //break;
+        case 22:
+            if (header->Length == 0 && (ispbatteryempty = false)) 
+            {
+                std::cout << "\n---\tPBATTERY structure appears to be empty\t---\n" << std::endl;
+                ispbatteryempty = true;
+                //continue;
+            }
+            //break;
+        case 41:
+            if (header->Length == 0 && (isnetdeviceempty == false))
+            {
+                std::cout << "\n---\tNETWORK DEVICE structure appears to be empty\t---\n" << std::endl;
+                isnetdeviceempty == true;    
+                return;
+            }
+            //break;
         }
-
-        LPBYTE nt = p + pHeader->Length;
+        
+        LPBYTE nt = p + header->Length;
         while (0 != (*nt | *(nt + 1))) nt++;
         nt += 2;
 
--- hwid.h
+++ hwid.h
@@ -2,19 +2,10 @@
 #include <Windows.h>
 #include <iostream>
 #include <tchar.h>
+#include <inttypes.h>
 
-
-
-
-
-
-namespace MACHINE 
-{
-	const wchar_t* SRN1;
-	const wchar_t* UUID1;
-	const wchar_t* SKU1;
-}
-
+#include "e115/XOR/xor.h"
+#include "e115/Obf/secrets.h"
 
 #ifdef UNICODE
 #define getHeaderString  getHeaderStringW
@@ -25,16 +16,15 @@ namespace MACHINE
 #endif
 
 
-
-
 class SMBIOSHWID
 {
 public:
-
+	
 	void ShowHwid();
-	/*void DumpSMBIOSStruct(void* Addr, UINT Len);*/
-private:
 
+private:
+#pragma pack(push)
+#pragma pack(1)
 	typedef struct _RawSMBIOSData
 	{
 		BYTE	Used20CallingMethod;
@@ -62,7 +52,7 @@ private:
 	} SMBIOSHEADER, * PSMBIOSHEADER;
 
 
-	typedef struct _TYPE_1_
+	typedef struct _TYPE_1_	//	0
 	{
 		SMBIOSHEADER	Header;
 		UCHAR	Manufacturer;
@@ -76,7 +66,7 @@ private:
 	} SystemInfo, * PSystemInfo;
 
 
-	typedef struct _TYPE_2_
+	typedef struct _TYPE_2_	//	1
 	{
 		SMBIOSHEADER	Header;
 		UCHAR	Manufacturer;
@@ -93,6 +83,87 @@ private:
 	} BoardInfo, * PBoardInfo;
 
 
+	typedef struct _TYPE_3_ 
+	{
+		SMBIOSHEADER Header;
+		UCHAR	Manufacturer;
+		UCHAR	Type;
+		UCHAR	Version;
+		UCHAR	SN;
+		UCHAR	AssetTag;
+		UCHAR	BootupState;
+		UCHAR	PowerSupplyState;
+		UCHAR	ThermalState;
+		UCHAR	SecurityStatus;
+		ULONG32	OEMDefine;
+		UCHAR	Height;
+		UCHAR	NumPowerCord;
+		UCHAR	ElementCount;
+		UCHAR	ElementRecordLength;
+		UCHAR	pElements;
+	} SystemEnclosure, * PSystemEnclosure;
+
+
+	typedef struct _TYPE_4_ 
+	{
+		SMBIOSHEADER Header;
+		UCHAR	SocketDesignation;
+		UCHAR	Type;
+		UCHAR	Family;
+		UCHAR	Manufacturer;
+		ULONG64 ID;
+		UCHAR	Version;
+		UCHAR	Voltage;
+		UINT16	ExtClock;
+		UINT16	MaxSpeed;
+		UINT16	CurrentSpeed;
+
+	} ProcessorInfo, * PProcessorInfo;
+
+
+	typedef struct _TYPE_17_ 
+	{
+		SMBIOSHEADER Header;
+		UINT16	PhysicalArrayHandle;
+		UINT16	ErrorInformationHandle;
+		UINT16	TotalWidth;
+		UINT16	DataWidth;
+		UINT16	Size;
+		UCHAR	FormFactor;
+		UCHAR	DeviceSet;
+		UCHAR	DeviceLocator;
+		UCHAR	BankLocator;
+		UCHAR	MemoryType;
+		UINT16	TypeDetail;
+		UINT16	Speed;
+		UCHAR	Manufacturer;
+		UCHAR	SN;
+		UCHAR	AssetTag;
+		UCHAR	PN;
+		UCHAR	Attributes;
+	} MemoryDevice, * PMemoryDevice;
+
+
+	typedef struct _TYPE_22_ 
+	{
+		SMBIOSHEADER Header;
+		UCHAR	Location;
+		UCHAR	Manufacturer;
+		UCHAR	Date;
+		UCHAR	SN;
+		UCHAR	DeviceName;
+		UCHAR   Chemistry;
+		UINT16  DesignCapacity;
+		UINT16  DesignVoltage;
+		UCHAR   SBDSVersionNumber;
+		UCHAR   MaximumErrorInBatteryData;
+		UINT16  SBDSSerialNumber;
+		UINT16	SBDSManufactureDate;
+		UCHAR   SBDSDeviceChemistry;
+		UCHAR   DesignCapacityMultiplie;
+		UINT32  OEM;
+	} PortableBattery, * PPortableBattery;
+
 	struct RawSMBIOSSystemInfo : public RawSMBIOSTable
 	{
 		BYTE    Manufacturer;
@@ -105,13 +176,36 @@ private:
 		BYTE    Family;
 	};
 
+
+	typedef struct _TYPE_41_
+	{
+		SMBIOSHEADER Header;
+		UINT8 ReferenceDesignation;
+		UINT8 DeviceType;
+		UINT16 DeviceTypeInstance;
+		UINT8 SegmentGroupNumber;
+		UINT8 BusNumber;
+		UINT8 DeviceFunctionNumber;
+		UINT8 DeviceID;
+		UINT32 VendorID;
+		UINT8 RevisionID;
+		UINT16 Flags;
+	} OnboardDevicesExtendedInformation, *POnboardDevicesExtendedInformation;
+
+
+	typedef UINT(WINAPI* PfGetSystemFirmwareTable)(_In_  DWORD  FirmwareTableProviderSignature,_In_  DWORD  FirmwareTableID,_Out_ PVOID  pFirmwareTableBuffer,_In_  DWORD  BufferSize);
+
+
 	const char* toPointString(void* p);
 	const char* LocateStringA(const char* str, UINT i);
 	const wchar_t* LocateStringW(const char* str, UINT i);
 	void DumpSMBIOSStruct(void* Addr, UINT Len);
-	bool DispatchStructType(PSMBIOSHEADER hdr);
-	bool ProcSysInfo(void* p);
-	bool LogicBoardInfo(void* p);
+	bool StartStructureRoutine(PSMBIOSHEADER hdr);
+	bool SystemDetails(void* p);
+	bool LogicBoardDetails(void* p);
+	bool ProcessorDetails(void* p);
+	bool MemoryDeviceDetails(void* p);
+	bool PortableBatteryDetails(void* p);
+	bool EnclosureDetails(void* p);
+	bool NetworkDetails(void* p);
 };
-
-

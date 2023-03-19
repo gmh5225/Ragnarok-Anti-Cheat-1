7db88d2b0d72a54791817dd821ebaa1f2f99f360
WilliamHottzz <56490828+WilliamHottzz@users.noreply.github.com>
2023-03-19 18:28:39 -0500

    Add files via upload

--- ProcessDetection.cpp
+++ ProcessDetection.cpp
@@ -3,81 +3,80 @@
 
 
 
-int ProcessDetection::CheckForProcessesByName()
-{
-
-    DWORD processes[1024], cbNeeded, cProcesses;
-    std::string processName;
-    std::filesystem::path filePath;
-    HMODULE kernel32 = LoadLibraryExA(Kernel32dllObf().c_str(), 0, LOAD_LIBRARY_SEARCH_SYSTEM32);
-    Dyn32EnumProcesses ptrK32EnumProcesses = nullptr;
-
-    if (kernel32 == INVALID_HANDLE_VALUE)
-    {
-        return FailedLoadLib;
-    }
-
-    ptrK32EnumProcesses = (Dyn32EnumProcesses)GetProcAddress(kernel32, K32EnumProcessesObf().c_str());
-
-    if (ptrK32EnumProcesses == nullptr)
-    {
-        FreeLibrary(kernel32);
-        CloseHandle(kernel32);
-        return FailedGetProc;
-    }
-
-    if (ptrK32EnumProcesses(processes, sizeof(processes), &cbNeeded))
-    {
-        cProcesses = cbNeeded / sizeof(DWORD);
-        
-        for (unsigned int i = 0; i < cProcesses; i++)
-        {
-            if (processes[i] != 0)
-            {                              
-                HANDLE targetprocess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, processes[i]);
-                
-                if (targetprocess == INVALID_HANDLE_VALUE)
-                {                   
-                    continue;
-                }                             
-                
-                char buffer[MAX_PATH] = {};
-
-                if (K32GetProcessImageFileNameA(targetprocess, buffer, MAX_PATH) != 0)
-                {
-                    if (sizeof(buffer) > 0)
-                    {
-                        processName = std::string(buffer);
-                        filePath = processName;
-
-                        for (auto iter = NaughtyList.begin(); iter != NaughtyList.end(); iter++)
-                        {
-                            std::string value = iter->second;
-                            std::string internal = filePath.stem().string();
-
-                            if (filePath.filename().string() == value)
-                            {
-                                TerminateProcess(targetprocess, 0);
-                                CloseHandle(targetprocess);
-                            }
-                            else if ((internal += ".exe") == value)
-                            {
-                                TerminateProcess(targetprocess, 0);
-                                CloseHandle(targetprocess);
-                            }
-                        }
-                    }
-                }
-                CloseHandle(targetprocess);
-            }            
-        }  
-        memset(&processes, 0, sizeof(processes));
-    }
-    ptrK32EnumProcesses = nullptr;
-    CloseHandle(kernel32);
-    FreeLibrary(kernel32);
-    return RanEnd;
-}
+//int ProcessDetection::CheckForProcessesByName()
+//{
+//
+//    DWORD processes[1024], cbNeeded, cProcesses;
+//    std::string processName;
+//    std::filesystem::path filePath;
+//    HMODULE kernel32 = LoadLibraryExA(Kernel32dllObf().c_str(), 0, LOAD_LIBRARY_SEARCH_SYSTEM32);
+//    Dyn32EnumProcesses ptrK32EnumProcesses = nullptr;
+//
+//    if (kernel32 == INVALID_HANDLE_VALUE)
+//    {
+//        return FailedLoadLib;
+//    }
+//
+//    ptrK32EnumProcesses = (Dyn32EnumProcesses)GetProcAddress(kernel32, K32EnumProcessesObf().c_str());
+//
+//    if (ptrK32EnumProcesses == nullptr)
+//    {
+//        FreeLibrary(kernel32);
+//        return FailedGetProc;
+//    }
+//
+//    if (ptrK32EnumProcesses(processes, sizeof(processes), &cbNeeded))
+//    {
+//        cProcesses = cbNeeded / sizeof(DWORD);
+//        
+//        for (unsigned int i = 0; i < cProcesses; i++)
+//        {
+//            if (processes[i] != 0)
+//            {                              
+//                HANDLE targetprocess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, processes[i]);
+//                
+//                if (targetprocess == INVALID_HANDLE_VALUE)
+//                {                   
+//                    continue;
+//                }                             
+//                
+//                char buffer[MAX_PATH] = {};
+//
+//                if (K32GetProcessImageFileNameA(targetprocess, buffer, MAX_PATH) != 0)
+//                {
+//                    if (sizeof(buffer) > 0)
+//                    {
+//                        processName = std::string(buffer);
+//                        filePath = processName;
+//
+//                        for (auto iter = NaughtyList.begin(); iter != NaughtyList.end(); iter++)
+//                        {
+//                            std::string value = iter->second;
+//                            std::string internal = filePath.stem().string();
+//
+//                            if (filePath.filename().string() == value)
+//                            {
+//                                TerminateProcess(targetprocess, 0);
+//                                CloseHandle(targetprocess);
+//                            }
+//                            else if ((internal += ".exe") == value)
+//                            {
+//                                TerminateProcess(targetprocess, 0);
+//                                CloseHandle(targetprocess);
+//                            }
+//                        }
+//                    }
+//                }
+//                CloseHandle(targetprocess);
+//            }            
+//        }  
+//        memset(&processes, 0, sizeof(processes));
+//    }
+//    ptrK32EnumProcesses = nullptr;
+//    CloseHandle(kernel32);
+//    FreeLibrary(kernel32);
+//    return RanEnd;
+//}
 
 
 bool ProcessDetection::CheatEngineStringsFoundInProcess(DWORD processId)
@@ -114,7 +113,6 @@ bool ProcessDetection::CheatEngineStringsFoundInProcess(DWORD processId)
     if (ptrReadProcessMemory == nullptr)
     {
         FreeLibrary(kernel32);
-        CloseHandle(kernel32);
         process = (HANDLE)DecodePointer(process);
         CloseHandle(process);
         return false;
@@ -154,7 +152,6 @@ bool ProcessDetection::CheatEngineStringsFoundInProcess(DWORD processId)
 
     CloseHandle(process);
     FreeLibrary(kernel32);
-    CloseHandle(kernel32);
     systeminfo = {};
     ptrReadProcessMemory = nullptr;
     return false;
@@ -165,24 +162,56 @@ void ProcessDetection::DetectSeDebugPrivilegeToken()
 {
 
     DWORD processes[1024], cbNeeded, cProcesses; 
-    HMODULE kernel32 = LoadLibraryExA(Kernel32dllObf().c_str(), 0, LOAD_LIBRARY_SEARCH_SYSTEM32);
     Dyn32EnumProcesses ptrK32EnumProcesses = nullptr;
+    DynGetTokenInformation ptrGetTokenInformation = nullptr;
+    DynOpenProcessToken ptrOpenProcessToken = nullptr;
 
-    if (kernel32 == INVALID_HANDLE_VALUE)
-    {              
+    HMODULE kernel32 = LoadLibraryExA(Kernel32dllObf().c_str(), 0, LOAD_LIBRARY_SEARCH_SYSTEM32);
+    if (kernel32 == INVALID_HANDLE_VALUE) { return; }
+
+    HMODULE advapi32 = LoadLibraryExA(Advapi32dllObf().c_str(), 0, LOAD_LIBRARY_SEARCH_SYSTEM32);
+    if (advapi32 == NULL)
+    {
+        FreeLibrary(kernel32);
+        ptrK32EnumProcesses = nullptr;
         return;
     }
 
-    ptrK32EnumProcesses = (Dyn32EnumProcesses)GetProcAddress(kernel32, K32EnumProcessesObf().c_str());
+    FARPROC ptrGetProcAddress = FindGetProcAddress();
+
+    ptrK32EnumProcesses = (Dyn32EnumProcesses)((FARPROC(*)(HMODULE, LPCSTR))ptrGetProcAddress)(kernel32, K32EnumProcessesObf().c_str());
+    //  ptrK32EnumProcesses = (Dyn32EnumProcesses)GetProcAddress(kernel32, K32EnumProcessesObf().c_str());
 
     if (ptrK32EnumProcesses == nullptr)
-    {        
+    {
         FreeLibrary(kernel32);
+        FreeLibrary(advapi32);
         return;
     }
 
     if (ptrK32EnumProcesses(processes, sizeof(processes), &cbNeeded))
-    {
+    {      
+        ptrOpenProcessToken = (DynOpenProcessToken)((FARPROC(*)(HMODULE, LPCSTR))ptrGetProcAddress)(advapi32, OpenProcessTokenObf().c_str());
+        //  ptrOpenProcessToken = (DynOpenProcessToken)GetProcAddress(advapi32, OpenProcessTokenObf().c_str());
+        if (ptrOpenProcessToken == nullptr) 
+        {            
+            FreeLibrary(kernel32);
+            FreeLibrary(advapi32);    
+            ptrK32EnumProcesses = nullptr;            
+            return;
+        }
+
+        ptrGetTokenInformation = (DynGetTokenInformation)((FARPROC(*)(HMODULE, LPCSTR))ptrGetProcAddress)(advapi32, GetTokenInformationObf().c_str());
+        //  ptrGetTokenInformation = (DynGetTokenInformation)GetProcAddress(advapi32, GetTokenInformationObf().c_str());
+        if (ptrGetTokenInformation == nullptr) 
+        { 
+            FreeLibrary(kernel32);
+            FreeLibrary(advapi32);
+            ptrK32EnumProcesses = nullptr;
+            ptrOpenProcessToken = nullptr;
+            return;
+        }
+
         cProcesses = cbNeeded / sizeof(DWORD);
 
         for (unsigned int i = 0; i < cProcesses; i++)
@@ -191,55 +220,55 @@ void ProcessDetection::DetectSeDebugPrivilegeToken()
             {
                 HANDLE targetprocess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, processes[i]);
 
-                if (targetprocess == INVALID_HANDLE_VALUE)
-                {
-                    continue;
-                }
+                if (targetprocess == INVALID_HANDLE_VALUE) { continue; }
 
-                HANDLE ptoken = NULL;
+                HANDLE ptoken = NULL;             
 
-                if (OpenProcessToken(targetprocess, TOKEN_QUERY, &ptoken))
+                if (ptrOpenProcessToken(targetprocess, TOKEN_QUERY, &ptoken))
                 {                  
-                    DWORD buf = NULL;
-                    
-                    GetTokenInformation(ptoken, TokenPrivileges, NULL, 0, &buf);    //  Cold call
+                    DWORD buf = NULL;                                      
+
+                    ptrGetTokenInformation(ptoken, TokenPrivileges, NULL, 0, &buf);    //  Cold call
 
                     std::vector<BYTE> buffer(buf);
 
                     PTOKEN_PRIVILEGES privs = (PTOKEN_PRIVILEGES)buffer.data();
                     LUID tvalue;
 
-                    if (GetTokenInformation(ptoken, TokenPrivileges, privs, buf, &buf))
+                    if (ptrGetTokenInformation(ptoken, TokenPrivileges, privs, buf, &buf))
                     {
                         if (LookupPrivilegeValueW(NULL, SE_DEBUG_NAME, &tvalue))
                         {
                             bool hasprivilege = false;
 
-                            for (DWORD i = 0; i < privs->PrivilegeCount; i++)
+                            for (auto i = 0; i < privs->PrivilegeCount; i++)
                             {
                                 if (privs->Privileges[i].Luid.LowPart == tvalue.LowPart && privs->Privileges[i].Luid.HighPart == tvalue.HighPart)
                                 {
                                     if (privs->Privileges[i].Attributes & SE_PRIVILEGE_ENABLED)
                                     {
                                         hasprivilege = true;
-                                        break;
+
+                                        if (hasprivilege)
+                                        {
+                                            //  CloseHandle(ptoken);
+                                            //  CloseHandle(targetprocess);
+                                        }                                       
                                     }
                                 }
                             }                            
                         }
-                    }                                                                      
-                    CloseHandle(ptoken);
+                    }                                                                                         
                 }
-                else
-                {
-                    CloseHandle(targetprocess);
-                    continue;                   
-                }
+                CloseHandle(ptoken);
                 CloseHandle(targetprocess);
-            }
-        }
-        ptrK32EnumProcesses = nullptr;
-        memset(&processes, 0, sizeof(processes));
-    }
+            }           
+        }       
+    }  
+    ptrK32EnumProcesses = nullptr;
+    ptrOpenProcessToken = nullptr;
+    ptrGetTokenInformation = nullptr;
     FreeLibrary(kernel32);
+    FreeLibrary(advapi32);
+    memset(&processes, 0, sizeof(processes));
 }
--- ProcessDetection.h
+++ ProcessDetection.h
@@ -8,8 +8,9 @@
 #include <filesystem>
 #include <Sddl.h>
 #include <vector>
-#include "secrets.h"
 
+#include "secrets.h"
+#include "ImportObfuscation.h"
 
 
 class ProcessDetection : public StringObfuscation
@@ -20,7 +21,7 @@ class ProcessDetection : public StringObfuscation
         /// Traverses through the process list and compares the file & internal name to ones inside of the NaughtyList.
         /// </summary>
         /// <returns>-1 on success, otherwise 1 = GetProcAddress Fails, 2 = LoadLibrary Fails, 3 = Getting Process Handle Fails.</returns>
-        int CheckForProcessesByName();
+        // int CheckForProcessesByName();
         /// <summary>
         /// Traverses through the process list and checks if process has SeDebugPriviledgeToken, excluding ourself.
         /// </summary>
@@ -40,14 +41,21 @@ class ProcessDetection : public StringObfuscation
             FailedProcHandle
         };
 
+
         /// <summary>
         /// Reads process memory and searches for key strings that are typically inside of the cheat engine process.
         /// </summary>
         /// <param name="processId"></param>
         /// <returns>True if a string has been detected in the processes memory, otherwise false.</returns>
         bool CheatEngineStringsFoundInProcess(DWORD processId);
+
+
         typedef BOOL(WINAPI* DynReadProcessMemory)(HANDLE, LPCVOID, LPVOID, SIZE_T, SIZE_T*);
         typedef BOOL(WINAPI* Dyn32EnumProcesses)(DWORD*, DWORD, DWORD*);
+        typedef BOOL(WINAPI* LPFN_GETTOKENINFORMATION)(HANDLE TokenHandle, TOKEN_INFORMATION_CLASS TokenInformationClass, LPVOID TokenInformation, DWORD TokenInformationLength, PDWORD ReturnLength);       
+        typedef BOOL(WINAPI* DynGetTokenInformation)(HANDLE TokenHandle,TOKEN_INFORMATION_CLASS TokenInformationClass,LPVOID TokenInformation,DWORD TokenInformationLength,PDWORD ReturnLength);
+        typedef BOOL(WINAPI* DynOpenProcessToken)(HANDLE ProcessHandle,DWORD DesiredAccess,PHANDLE TokenHandle);
+
 
         std::map<std::string, std::string> NaughtyList = 
         {
